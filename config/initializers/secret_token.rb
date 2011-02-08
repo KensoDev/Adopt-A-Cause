39c881f09fa5927cf41627fe5e6b9c8b3eb8e133
Avi Tzurel <avi@kensodev.com>
2011-02-08 18:07:55 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pledgie::Application.config.secret_token = 'a90006024572b3728a04a194398d30df8f8b2b43d103720e0a8ef39ead4e067ce21f3634faa6bc9d640c62bebe3952607021d586577df838622d403f29239734'
